9f392544923f3a853aaea11773e5e8b64b035f9f
Andrew Peace <apeace@users.noreply.github.com>
2019-03-30 22:14:58 -0400

    highest

--- util.py
+++ util.py
@@ -25,3 +25,31 @@ def lowest(members):
             lowest_member = member
 
     return lowest_member
+
+def highest(members):
+    """
+    Given a dict, returns the key with the highest value.
+    If there is a tie for highest value, returns None.
+    """
+    if len(members) == 0:
+        return None
+
+    reverse_members = {}
+    max = None
+
+    for member in members:
+        reverse_members[members[member]] = member
+        if max is None:
+            max = members[member]
+            continue
+        if members[member] > max:
+            max = members[member]
+
+    highest_member = None
+    for member in members:
+        if members[member] == max:
+            if highest_member is not None:
+                return None
+            highest_member = member
+
+    return highest_member
--- util_test.py
+++ util_test.py
@@ -24,3 +24,28 @@ def test_lowest():
         "ron": 1,
     }
     assert util.lowest(members) is None
+
+def test_highest():
+    members = {}
+    assert util.highest(members) is None
+
+    members = {
+        "fred": 1,
+        "george": 2,
+        "ron": 3,
+    }
+    assert util.highest(members) == "ron"
+
+    members = {
+        "fred": 1,
+        "george": 3,
+        "ron": 3,
+    }
+    assert util.highest(members) is None
+
+    members = {
+        "fred": 1,
+        "george": 1,
+        "ron": 1,
+    }
+    assert util.highest(members) is None
