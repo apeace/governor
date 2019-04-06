8bda8e519ee6330531d7d88f58e207fb672f62cc
Andrew Peace <apeace@users.noreply.github.com>
2019-04-06 13:53:56 -0400

    Fix game_test not actually running any moves

--- game_test.py
+++ game_test.py
@@ -8,8 +8,14 @@ def test_random_game():
     rand = engine.RandomEngine(l)
     state = kingsburg.State()
     g = game.Game(rand, state)
+    g.setup()
 
+    num_ticks = 0
     ## TODO actually test some states
-    for i in range(0, 1000):
-        g.tick()
+    for i in range(0, 5000):
+        if g.tick():
+            return
+        num_ticks += 1
 
+    assert num_ticks > 20
+    assert kingsburg.PHASES[g.state.phase] == kingsburg.PHASE_KINGS_REWARD
