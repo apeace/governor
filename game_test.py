84c10526f031adbf4b1a13007a04c87d69986558
Andrew Peace <apeace@users.noreply.github.com>
2019-04-06 14:02:09 -0400

    Cleanup

--- game_test.py
+++ game_test.py
@@ -10,12 +10,9 @@ def test_random_game():
     g = game.Game(rand, state)
     g.setup()
 
-    num_ticks = 0
     ## TODO actually test some states
     for i in range(0, 5000):
         if g.tick():
             return
-        num_ticks += 1
 
-    assert num_ticks > 20
     assert kingsburg.PHASES[g.state.phase] == kingsburg.PHASE_KINGS_REWARD
